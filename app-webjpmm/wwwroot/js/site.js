07357fbe2fa47ae9da94ac094b36fd30db6ab281
JHANPIERRE <jmunozmanayay@gmail.com>
2024-03-08 17:04:30 -0500

    9

--- app-webjpmm/wwwroot/js/site.js
+++ app-webjpmm/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portafolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'rigth' });
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Paginas Web', 'Sistemas web', 'Aplicaciones Escritorio'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
